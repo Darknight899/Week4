380588732a32a70298081ec28fc076430d9cc370
Darknight899 <kyte_2@hotmail.com>
2024-11-13 11:48:11 -0500

    Created data_types.py in week 4
    Wrote an example of code for a string, list, and tuple

---
+++ data_types.py
@@ -0,0 +1,11 @@
+# Define a string
+my_string = "Hello, my name is Patrick!"
+# Define a list 
+my_list = [3,12,19,22,28,29]
+# Define a tuple
+my_typle = (100,200,300)
+# Output the values 
+print("String: my_string")
+print("List: my_list")
+print("Tuple: my_tuple")
+
